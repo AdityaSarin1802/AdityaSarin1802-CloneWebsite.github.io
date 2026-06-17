66e4a7594696dd87bec71bbcddd653741ee61729
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-06-18 03:23:01 +0530

    updated interface

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 let cart = 0;
 
-function addToCart(){
+function addToCart() {
     cart++;
     document.getElementById("cartCount").innerText = cart;
 }
 
-function searchProducts(){
-    let input =
-    document.getElementById("searchInput")
-    .value
-    .toLowerCase();
-    let cards =
-    document.querySelectorAll(".card");
-    cards.forEach(function(card){
-        let title =
-        card.querySelector("h3")
-        .innerText
+function searchProducts() {
+
+    let input = document
+        .getElementById("searchInput")
+        .value
         .toLowerCase();
+
+    let cards = document.querySelectorAll(".card");
+
+    cards.forEach(function(card) {
+
+        let title = card
+            .querySelector("h2")
+            .innerText
+            .toLowerCase();
+
         if(title.includes(input)){
             card.style.display = "block";
         }
         else{
             card.style.display = "none";
         }
+
     });
+
 }
